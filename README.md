300b15905a9a68858f531694c670c59edb9d178b
Mark Clarkson <mark.clarkson@smorg.co.uk>
2016-04-12 15:00:19 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# obdi-nettools
+Obdi plugin providing the base nettools plugin required by some plugins.
+
+If this plugin is not installed with other nettools plugins then they will continue to work as normal, and their REST interfaces can be used, but there will be no way to view the nettools plugins in the Web Interface.
---
+++ install_plugin.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Obdi - a REST interface and GUI for deploying software
+# Copyright (C) 2014  Mark Clarkson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# saltconfigserver plugin
+
+[[ -z $guid ]] && {
+  echo "ERROR: environment variable 'guid' must be set."
+  exit 1
+}
+
+proto="https"
+opts="-k -s" # don't check ssl cert, silent
+ipport="127.0.0.1:443"
+
+# salt stub
+
+curl -k -d '{
+    "Name":"nettools",
+    "Desc":"Network Tools main sidebar item",
+    "HasView":2,
+    "Parent":""
+}' $proto://$ipport/api/admin/$guid/plugins
+
